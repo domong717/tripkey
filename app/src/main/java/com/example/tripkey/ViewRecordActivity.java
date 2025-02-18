c8a10d138c744bcab551e37bed69bf17fe3cc5be
shinyumin1 <tlsdbals123@naver.com>
2025-02-19 02:04:50 +0900

    기록 추가_사진

--- app/src/main/java/com/example/tripkey/ViewRecordActivity.java
+++ app/src/main/java/com/example/tripkey/ViewRecordActivity.java
@@ -1,15 +1,4 @@
 package com.example.tripkey;
 
-import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class ViewRecordActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_record); // XML 파일과 연결
-    }
+public class ViewRecordActivity {
 }
-
-
