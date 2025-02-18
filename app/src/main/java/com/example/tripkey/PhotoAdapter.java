3b0b1b7788ce7323eef6ab65576b579e0d33df30
shinyumin1 <tlsdbals123@naver.com>
2025-02-19 02:08:36 +0900

    기록 추가_사진

--- app/src/main/java/com/example/tripkey/PhotoAdapter.java
+++ app/src/main/java/com/example/tripkey/PhotoAdapter.java
@@ -1,4 +1,57 @@
 package com.example.tripkey;
 
-public class PhotoAdapter {
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class PhotoAdapter extends RecyclerView.Adapter<PhotoAdapter.PhotoViewHolder> {
+    private ArrayList<Uri> photoList;
+
+    public PhotoAdapter(ArrayList<Uri> photoList) {
+        this.photoList = photoList;  // Uri 타입으로 설정
+    }
+
+    @NonNull
+    @Override
+    public PhotoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_photo, parent, false);
+        return new PhotoViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PhotoViewHolder holder, int position) {
+        Uri photoUri = photoList.get(position);
+        Glide.with(holder.imageView.getContext())
+                .load(photoUri)
+                .into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return photoList.size();
+    }
+
+    public void updatePhotoList(ArrayList<Uri> newPhotos) {
+        photoList.clear();
+        photoList.addAll(newPhotos);
+        notifyDataSetChanged();
+    }
+
+    static class PhotoViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+
+        public PhotoViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.photoImageView);
+        }
+    }
 }
